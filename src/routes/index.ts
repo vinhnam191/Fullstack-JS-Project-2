125432597d0ed8402ee029e93526fa3145c1fd90
vinhnam191 <vinhnam191@gmail.com>
2023-10-03 22:34:45 +0700

    code formating

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,15 @@
 import express from 'express';
 const routers = express.Router();
-import productRoute from '../routes/api/productRoute'
+import productRoute from '../routes/api/productRoute';
 import userRoute from './api/userRoute';
-import orderRoute from './api/orderRoute'
+import orderRoute from './api/orderRoute';
 
 routers.get('/', (req, res) => {
-    res.send(
-        'Welcome to Project 2 of Udacity'
-    );
+  res.send('Welcome to Project 2 of Udacity');
 });
 
 routers.use('/products', productRoute);
-routers.use('/users', userRoute)
-routers.use('/orders', orderRoute)
+routers.use('/users', userRoute);
+routers.use('/orders', orderRoute);
 
 export default routers;
